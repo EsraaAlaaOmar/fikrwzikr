14e51b0b5ec7e6f94ddc94db9be73c092e68c150
EsraaAlaaOmar <esraa.safa55@gmail.com>
2024-03-10 12:02:50 +0200

    update terms

--- pages/AddVideo.tsx
+++ pages/AddVideo.tsx
@@ -4,7 +4,7 @@ import { useMutation  } from 'react-query';
 import Loader from '../components/competitioncomponents/Loader';
 import { useRouter } from 'next/router';
 import { MdOutlineKeyboardArrowLeft } from 'react-icons/md'
-import CompetitonTerms from '../components/competitioncomponents/competitonTerms';
+import CompetitonTerms from '../components/competitioncomponents/CompetitonTerms';
 const axios = require("axios");
 
 
