04f0ed1726cfdfbdc63d7c8848a108cec5841727
EsraaAlaaOmar <esraa.safa55@gmail.com>
2024-03-24 17:31:56 +0200

    update sbha

--- components/Zikr.tsx
+++ components/Zikr.tsx
@@ -14,7 +14,7 @@ const Zikr: React.FC<MyComponentProps> = ({ zikrName})  => {
       
       <button className="zkr-button" id="sbha" onClick={() =>counter !==33 && setCounter(counter + 1)} style={{ outline:"none"}}>{ counter }</button>
        {counter !==33 && counter !==0 && <div onClick={()=>setCounter(0)} className="sbha-reset">بداية العد  </div>}
-       {counter ==33 && <div onClick={()=>setCounter(0)} className="sbha-reset"> لقد اكملت التسبيحة  .. اضغط لبدأ تسبيحة جديدة  </div>}
+       {counter ==33 && <div onClick={()=>setCounter(0)} className="sbha-reset"> لقد اكملت التسبيحة ..  <span>اضغط لبدأ تسبيحة جديدة </span> </div>}
        
     </div>
 
