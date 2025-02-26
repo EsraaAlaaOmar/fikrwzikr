fe6e785ee232f398515bc5218058244447f8a6cc
EsraaAlaaOmar <esraa.safa55@gmail.com>
2025-02-26 12:47:10 +0400

    solve     from k2nk traho

---
+++ pages/Category/[id]/index.tsx
@@ -0,0 +1,104 @@
+import React from 'react'
+
+const _Category = () => {
+  return (
+    <>
+         <div className="single-header category-header">
+                     <div className="container text-center">
+                         <div className="animatable fadeInUp">
+                             <a href="Index">
+                                 <img src="/images/menu-logo.png" className="center-block" alt="logo" /></a>
+                                 <h3> القران الكريم</h3>
+                         
+                              
+                         </div>
+                     </div>
+                 </div>
+
+                 <section className="category-content">
+                     <div className="container">
+                         <ul className="list-unstyled">
+                            
+                             <h5>
+                                 <span id="lblCategoryContent">القرآن الكريم</span></h5>
+                                 <li className="panel"><a id="lblCategoriesList"></a>
+                                     <a >الختمات القرآنية <span className="caret"></span></a>
+                                     <ul  id="div_C_2">
+                                        <li><a href="Videos.aspx?id=3">الختمة الدولية (67 قارئا من 67 دولة) </a>
+                                     </li>
+                                      <li><a href="Videos.aspx?id=4">ختمة الشيخ محمود الخشت (مصر) </a></li>
+                                       <li><a href="Videos.aspx?id=5">ختمة (وقرآنا فرقناه) المرتلة – قراء متنوعون </a></li>
+                                        <li><a href="Videos.aspx?id=6">ختمة (وقرآنا فرقناه) المجودة – قراء متنوعون </a></li> 
+                                        </ul>
+                                        </li>
+                                
+                                        <li className="panel"><a href="#div_C_7" data-toggle="" data-parent="#accordion-main" aria-expanded="false">قراء الأقطار <span className="caret"></span></a>
+                                        <ul className="" id="div_C_7">
+                                            <li><a href="Videos.aspx?id=8">القارئ الشيخ معاذ الدويك (المغرب) </a></li>
+                                             <li><a href="Videos.aspx?id=9">القارئ الشيخ محمد خالد (اليمن) </a></li> 
+                                             <li><a href="Videos.aspx?id=10">القارئ الشيخ أحمد الحداد (مصر) </a></li>
+                                              </ul></li>
+
+                                           
+                                           
+                                              
+                                               <li className="panel"><a href="#div_C_11" data-toggle="" data-parent="#accordion-main" aria-expanded="false">آيات المناسبات <span className="caret"></span></a>
+                                              <ul className="" id="div_C_11"><li><a href="Videos.aspx?id=12">آيات الحج في القرآن الكريم </a></li>
+                                               <li><a href="Videos.aspx?id=13">آيات الصيام في القرآن الكريم </a></li> </ul></li>
+                                              
+                                               <li className="panel"><a href="#div_C_14" data-toggle="" data-parent="#accordion-main" aria-expanded="false"> آيات النبي ﷺ في القرآن الكريم <span className="caret"></span></a><ul className="" id="div_C_14"><li>
+                                                <a href="Videos.aspx?id=15">آيات النبي ﷺ بصوت شباب قراء العالم المعاصرين </a></li>
+                                                 <li><a href="Videos.aspx?id=16">آيات النبي ﷺ بصوت كبار قراء العالم المعاصرين </a></li> 
+                                                 <li><a href="Videos.aspx?id=17">آيات النبي ﷺ بصوت كبار القراء الراحلين </a></li> </ul></li>
+                             <ul className="collapse in parent-ul" aria-expanded="true">
+                                 <div id="accordion-main" role="tablist" aria-multiselectable="true">
+                                     <a id="lblCategoriesList"></a>
+                                     <li className="panel"><a id="lblCategoriesList"></a>
+                                     <a >الختمات القرآنية <span className="caret"></span></a>
+                                     <ul className="" id="div_C_2">
+                                        <li><a href="Videos.aspx?id=3">الختمة الدولية (67 قارئا من 67 دولة) </a>
+                                     </li>
+                                      <li><a href="Videos.aspx?id=4">ختمة الشيخ محمود الخشت (مصر) </a></li>
+                                       <li><a href="Videos.aspx?id=5">ختمة (وقرآنا فرقناه) المرتلة – قراء متنوعون </a></li>
+                                        <li><a href="Videos.aspx?id=6">ختمة (وقرآنا فرقناه) المجودة – قراء متنوعون </a></li> 
+                                        </ul>
+                                        </li>
+                                        <li className="panel"><a href="#div_C_7" data-toggle="" data-parent="#accordion-main" aria-expanded="false">قراء الأقطار <span className="caret"></span></a>
+                                        <ul className="" id="div_C_7">
+                                            <li><a href="Videos.aspx?id=8">القارئ الشيخ معاذ الدويك (المغرب) </a></li>
+                                             <li><a href="Videos.aspx?id=9">القارئ الشيخ محمد خالد (اليمن) </a></li> 
+                                             <li><a href="Videos.aspx?id=10">القارئ الشيخ أحمد الحداد (مصر) </a></li>
+                                              </ul></li>
+                                              <li className="panel"><a href="#div_C_11" data-toggle="" data-parent="#accordion-main" aria-expanded="false">آيات المناسبات <span className="caret"></span></a>
+                                              <ul className="" id="div_C_11"><li><a href="Videos.aspx?id=12">آيات الحج في القرآن الكريم </a></li>
+                                               <li><a href="Videos.aspx?id=13">آيات الصيام في القرآن الكريم </a></li> </ul></li>
+                                               <li className="panel"><a href="#div_C_14" data-toggle="" data-parent="#accordion-main" aria-expanded="false"> آيات النبي ﷺ في القرآن الكريم <span className="caret"></span></a><ul className="" id="div_C_14"><li>
+                                                <a href="Videos.aspx?id=15">آيات النبي ﷺ بصوت شباب قراء العالم المعاصرين </a></li>
+                                                 <li><a href="Videos.aspx?id=16">آيات النبي ﷺ بصوت كبار قراء العالم المعاصرين </a></li> 
+                                                 <li><a href="Videos.aspx?id=17">آيات النبي ﷺ بصوت كبار القراء الراحلين </a></li> </ul></li>
+                                 </div>
+                             </ul>
+                         </ul>
+                     </div>
+                 </section>
+                 {/* <section className="category-content">
+                     <div className="container">
+                         <ul className="list-unstyled">
+                             
+                         <h5> القران الكريم</h5>
+                             <ul className="collapse in parent-ul" aria-expanded="true">
+                                
+                                 <div id="accordion-main" role="tablist" aria-multiselectable="true">
+                                 <a id="lblCategoriesList">اسراء</a>
+                                 اسراء علاء
+                                 </div>
+                             </ul>
+                         </ul>
+                     </div>
+                 </section> */}
+      
+    </>
+  )
+}
+
+export default _Category
