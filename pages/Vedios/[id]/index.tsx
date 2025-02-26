f82be8cee1edf70fa554f3cb20806d07941a4569
EsraaAlaaOmar <esraa.safa55@gmail.com>
2025-02-26 12:43:44 +0400

    update

---
+++ pages/Vedios/[id]/index.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+          <div className="single-header">
+     <div className="container text-center">
+      <div className="animatable fadeInUp"><a href="Index"><img src="/images/menu-logo.png" className="center-block" alt="logo" /></a>
+        <a id="lblcategoryheader" >
+
+        <ol className="breadcrumb"><li>القرآن الكريم</li><li>الختمات القرآنية</li><li className="active">الختمة الدولية (67 قارئا من 67 دولة)</li></ol>
+        </a>
+      </div>    
+     </div>  
+    </div>
+    <section className="single-content">
+     <div className="container">
+      <div className="heading"><h2>الفيديوهات</h2></div>
+      <div className="row">
+      <div className="col-md-4 item">
+                      <a href="VideoDetails?id=10229">
+                          <img alt="من سورة البقرة بصوت القارئ/ أولي الأمر زين الدين..." className="img-responsive" src="https://ka2naktraho.com/contents/Content/islamic-10229.jpg" style={{opacity: "1", visibility: "visible"}}/>
+                          <h3>من سورة البقرة بصوت القارئ/ أولي الأمر زين الدين.. ... </h3>
+                          <span className="free" style={{opacity: "1", visibility: "visible", display: "none"}}></span>
+                      </a>
+                  </div>
+                  <div className="col-md-4 item">
+                      <a href="VideoDetails?id=10229">
+                          <img alt="من سورة البقرة بصوت القارئ/ أولي الأمر زين الدين..." className="img-responsive" src="https://ka2naktraho.com/contents/Content/islamic-10229.jpg" style={{opacity: "1", visibility: "visible"}}/>
+                          <h3>من سورة البقرة بصوت القارئ/ أولي الأمر زين الدين.. ... </h3>
+                          <span className="free" style={{opacity: "1", visibility: "visible", display: "none"}}></span>
+                      </a>
+                  </div>
+                  <div className="col-md-4 item">
+                      <a href="VideoDetails?id=10226">
+                          <img alt="القارئ/  محمد النجم ثاقب - الفاتحة وأول البقرة" className="img-responsive" src="https://ka2naktraho.com/contents/Content/islamic-10226.jpg" style={{opacity: "1", visibility: "visible"}}/>
+                          <h3>القارئ/  محمد النجم ثاقب - الفاتحة وأول البقرة </h3>
+                          <span className="free" style={{opacity: "1", visibility: "visible"}}>مجاناً</span>
+                      </a>
+                  </div>
+                  <div className="col-md-4 item">
+                      <a href="VideoDetails?id=10229">
+                          <img alt="من سورة البقرة بصوت القارئ/ أولي الأمر زين الدين..." className="img-responsive" src="https://ka2naktraho.com/contents/Content/islamic-10229.jpg" style={{opacity: "1", visibility: "visible"}}/>
+                          <h3>من سورة البقرة بصوت القارئ/ أولي الأمر زين الدين.. ... </h3>
+                          <span className="free" style={{opacity: "1", visibility: "visible", display: "none"}}></span>
+                      </a>
+                  </div>
+      </div>   
+     </div>  
+    </section>
+    </div>
+  )
+}
+
+export default index
